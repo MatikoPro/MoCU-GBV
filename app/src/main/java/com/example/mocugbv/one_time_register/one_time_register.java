2ad5300e3a42e77a4dba1a2deba3d962b547cffb
nac <matikopascal96@gmail.com>
2024-03-19 23:07:27 +0300

    VERSION1

---
+++ app/src/main/java/com/example/mocugbv/one_time_register/one_time_register.java
@@ -0,0 +1,16 @@
+package com.example.mocugbv.one_time_register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mocugbv.R;
+
+public class one_time_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_time_register);
+    }
+}
